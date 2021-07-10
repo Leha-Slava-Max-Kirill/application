3a53f2809c948975bfc6d6b2751ae4c66fce2c82
Suraba03 <74858533+Suraba03@users.noreply.github.com>
2021-07-10 07:53:00 +0300

    пока ничего не работает

---
+++ server/controllers/tableController.js
@@ -0,0 +1,40 @@
+const {Table} = require('../models/models')
+const ApiError = require('../error/ApiError')
+
+
+class TableController {
+    /*async create(req, res) {
+        const {name, volume} = req.body
+        const table = await Table.create({name, volume})
+        return res.json(table)
+    }*/
+    async create(req, res, next) {
+        try {
+            const {name, volume} = req.body
+            //const {img} = req.files
+
+            const table = await Table.create({name, volume}) //
+
+            return res.json(table) //
+        } catch (e) {
+            next(ApiError.badRequest(e.message))
+        }
+    }
+
+    async getAll(req, res) {
+        const {name, volume} = req.query
+        let tables = await Table.findAll({name, volume})
+        return res.json(tables)
+        
+        /*try {
+            const {name, volume} = req.query
+            let tables = await Table.findAll({where:{name, volume}})
+            return res.json(tables)
+        } catch (e) {
+            next(ApiError.badRequest(e.message))
+        }*/
+    }
+
+}
+
+module.exports = new TableController()
