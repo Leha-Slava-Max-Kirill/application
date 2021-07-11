c3426525455b99b165253fdc6d3a5b4100528693
Suraba03 <74858533+Suraba03@users.noreply.github.com>
2021-07-11 13:03:40 +0300

    Delete server directory

--- server/routes/tableRouter.js
+++
@@ -1,9 +0,0 @@
-const Router = require('express')
-const router = new Router()
-const tableController = require('../controllers/tableController')
-
-router.post('/', tableController.create)
-router.get('/', tableController.getAll)
-//router.delete('/', )
-
-module.exports = router
